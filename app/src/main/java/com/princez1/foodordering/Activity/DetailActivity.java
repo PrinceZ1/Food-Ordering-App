ffa5836c94755b469089e0aa82c8ebc33ab39632
NguyenDepTrai <nguyen.tt1703@gmail.com>
2025-03-29 11:25:13 +0700

    add details food layout

---
+++ app/src/main/java/com/princez1/foodordering/Activity/DetailActivity.java
@@ -0,0 +1,23 @@
+package com.princez1.foodordering.Activity;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.princez1.foodordering.R;
+import com.princez1.foodordering.databinding.ActivityDetailBinding;
+
+public class DetailActivity extends AppCompatActivity {
+ActivityDetailBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityDetailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+}
